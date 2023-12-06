b5ad14cc3e234fbd63789c5fc711bb8d40fdf91e
ChuEating <chueating1005.cs11@nycu.edu.tw>
2023-12-06 18:00:38 +0800

    HW2 Modify

--- HW2/SQL Query/4a.sql
+++ HW2/SQL Query/4a.sql
@@ -1,48 +1,60 @@
-WITH StringencyIndex(Date, Maximum, Minimum) AS(
+WITH StringencyIndex(Date, Continent_Code, Maximum, Minimum) AS(
 	SELECT
 		Date,
+		Continent_Code,
 		MAX(StringencyIndex_Average_ForDisplay),
 		Min(StringencyIndex_Average_ForDisplay)
 	FROM
-		Indices
+		Indices NATURAL JOIN Country_Continent
 	WHERE 
-		Date = 20220401 OR Date = 20210401 OR Date = 20200401
+		Date = 20200401 OR Date = 20210401 OR Date = 20220401 
 	GROUP BY
-		Date
+		Date, Continent_Code
 	),
 	CountryCode_Index AS(
 	SELECT 
 		S.Date,
-		MaxCountryCode.Two_Letter_Country_Code AS Max_CountryCode,
-		MinCountryCode.Two_Letter_Country_Code As Min_CountryCode
+		S.Continent_Code,
+		S.Maximum,
+		MaxCountry_Continent.Two_Letter_Country_Code AS Max_CountryCode,
+		S.Minimum,
+		MinCountry_Continent.Two_Letter_Country_Code AS Min_CountryCode
 	FROM
 		StringencyIndex AS S
-		LEFT JOIN Indices AS MaxCountryCode 
-			ON (S.Maximum = MaxCountryCode.StringencyIndex_Average_ForDisplay AND S.Date = MaxCountryCode.Date)
-		LEFT JOIN Indices AS MinCountryCode 
-			ON (S.Minimum = MinCountryCode.StringencyIndex_Average_ForDisplay AND S.Date = MinCountryCode.Date)
+		LEFT JOIN (Indices NATURAL JOIN Country_Continent) AS MaxCountry_Continent
+			ON (S.Maximum = MaxCountry_Continent.StringencyIndex_Average_ForDisplay 
+				AND S.Date = MaxCountry_Continent.Date
+			   	AND S.Continent_Code = MaxCountry_Continent.Continent_Code)
+		LEFT JOIN (Indices NATURAL JOIN Country_Continent) AS MinCountry_Continent
+			ON (S.Minimum = MinCountry_Continent.StringencyIndex_Average_ForDisplay 
+				AND S.Date = MinCountry_Continent.Date
+			   	AND S.Continent_Code = MinCountry_Continent.Continent_Code)
 	ORDER BY
-		S.Date
+		S.Date, S.Continent_Code
 	),
 	Country_N_Continent AS(
 	SELECT
 		Countries.Two_Letter_Country_Code,
 		Countries.Country_Name,
+		Continents.Continent_Code,
 		Continents.Continent_Name
 	FROM
 		Countries NATURAL JOIN Country_Continent NATURAL JOIN Continents
 	)
 SELECT
 	S.Date,
-	MaxCountryName.Continent_Name AS Max_CountryName,
-	MaxCountryName.Country_Name AS Max_CotinentName,
-	MinCountryName.Continent_Name AS Min_CountryName,
-	MinCountryName.Country_Name AS Min_CotinentName
+	MaxCountryName.Continent_Name,
+	S.Maximum AS Max_Stringency_Index, 
+	MaxCountryName.Country_Name AS Max_Country_Name,
+	S.Minimum AS Min_Stringency_Index,
+	MinCountryName.Country_Name AS Min_Country_Name
 FROM
 	CountryCode_Index AS S
 	LEFT JOIN Country_N_Continent AS MaxCountryName
-		ON S.Max_CountryCode = MaxCountryName.Two_Letter_Country_Code
+		ON (S.Max_CountryCode = MaxCountryName.Two_Letter_Country_Code
+			AND S.Continent_Code = MaxCountryName.Continent_Code)
 	LEFT JOIN Country_N_Continent AS MinCountryName
-		ON S.Min_CountryCode = MinCountryName.Two_Letter_Country_Code
+		ON (S.Min_CountryCode = MinCountryName.Two_Letter_Country_Code
+			AND S.Continent_Code = MinCountryName.Continent_Code)
 ORDER BY
-	S.Date
+	S.Date, MaxCountryName.Continent_Name
